d416db1fe63adf5f5ab90e6607c0839135a94aa8
Aden <aden.mccarville@gmail.com>
2023-10-18 18:53:19 -0400

    Created assertObjectsEqual function to mirror assertArraysEqual function, etc. for Objects

---
+++ assertObjectsEqual.js
@@ -0,0 +1,58 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) { // Immediately return false if arrays are of different length
+    return false;
+  }
+
+  for (let x = 0; x < arr1.length; x++) { // Loop to iterate through each element of both arrays and return false if it finds a pair that does not match
+    if (arr1[x] !== arr2[x]) {
+      return false;
+    }
+  }
+  return true; // If the loop completes, the arrays match and we can return true
+};
+
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (let key of keys1) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (eqArrays(object1[key], object2[key]) === false) {
+        return false;
+      } else {
+        continue;
+      }
+    }
+    if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertObjectsEqual = function(object1, object2) {
+  const inspect = require('util').inspect;
+  if (eqObjects(object1, object2)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(object1)} === ${inspect(object2)}`)
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(object1)} !== ${inspect(object2)}`);
+  }
+};
+
+const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+assertObjectsEqual(multiColorShirtObject, anotherMultiColorShirtObject);
+
+const mug = {size: 500, color: "red", material: "porcelain"};
+const mug2 = {size: 500, color: "red", material: "steel"};
+assertObjectsEqual(mug, mug2);
+
+const mug3 = {size: 500, color: "red", material: "porcelain"};
+const mug4 = {size: 500, color: "red"};
+assertObjectsEqual(mug3, mug4);
