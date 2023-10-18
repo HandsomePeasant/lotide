c900b981b98766ecd3200bb2234dd0d9bcaf517f
Aden <aden.mccarville@gmail.com>
2023-10-17 21:06:31 -0400

    Changed pass / fail messages to include the compared arrays

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -15,9 +15,9 @@ const eqArrays = function(arr1, arr2) {
 // Function will print pass or fail message to console depending on the result returned by the eqArrays function
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2) === true) {
-    console.log(`✅✅✅ Assertion Passed: arrays are identical`)
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`)
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed: arrays are NOT identical`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
   }
 };
 
