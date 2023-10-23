4f8f1eb527c1e14c7a22dfcc702777e5f7ef7f8e
Aden <aden.mccarville@gmail.com>
2023-10-23 19:09:42 -0400

    Began Mochai & Chai implementation beginning with headTest

--- test/headTest.js
+++ test/headTest.js
@@ -1,9 +1,20 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-assertEqual(head([5,6,7]), 5); // => Pass
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); // => Pass
-assertEqual(head([1, 2, 3]), "One"); // => Fail
-assertEqual(head(["one", "two", "three"]), 1); // => Fail
-assertEqual(head([1]), 1); // => Pass
-assertEqual(head([]), undefined); // => Pass
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), 'Hello');
+  });
+
+  it("returns undefined when given an empty array", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+});
