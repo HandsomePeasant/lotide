61822e5f86547fb53ec7781eb4cb75cbff2611ee
Aden <aden.mccarville@gmail.com>
2023-10-23 21:33:36 -0400

    assertObjectsEqual now exportable

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,40 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) { // Immediately return false if arrays are of different length
-    return false;
-  }
-
-  for (let x = 0; x < arr1.length; x++) { // Loop to iterate through each element of both arrays and return false if it finds a pair that does not match
-    if (arr1[x] !== arr2[x]) {
-      return false;
-    }
-  }
-  return true; // If the loop completes, the arrays match and we can return true
-};
-
-
-// Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
-const eqObjects = function(object1, object2) {
-  const keys1 = Object.keys(object1);
-  const keys2 = Object.keys(object2);
-  if (keys1.length !== keys2.length) {
-    return false;
-  }
-
-  for (let key of keys1) {
-    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      if (eqArrays(object1[key], object2[key]) === false) {
-        return false;
-      } else {
-        continue;
-      }
-    }
-    if (object1[key] !== object2[key]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqObjects = require('./eqObjects');
 
 const assertObjectsEqual = function(object1, object2) {
   const inspect = require('util').inspect;
@@ -45,14 +9,4 @@ const assertObjectsEqual = function(object1, object2) {
   }
 };
 
-const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
-const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
-assertObjectsEqual(multiColorShirtObject, anotherMultiColorShirtObject);
-
-const mug = {size: 500, color: "red", material: "porcelain"};
-const mug2 = {size: 500, color: "red", material: "steel"};
-assertObjectsEqual(mug, mug2);
-
-const mug3 = {size: 500, color: "red", material: "porcelain"};
-const mug4 = {size: 500, color: "red"};
-assertObjectsEqual(mug3, mug4);
+module.exports = assertObjectsEqual;
