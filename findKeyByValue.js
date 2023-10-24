5a1eb271a32655f90543e9ea4da33c39f0a8ddd7
Aden <aden.mccarville@gmail.com>
2023-10-23 21:40:52 -0400

    findKeyByValue now exportable

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const findKeyByValue = function(obj, val) {
   const keys = Object.keys(obj);
   const values = Object.values(obj);
@@ -16,15 +8,4 @@ const findKeyByValue = function(obj, val) {
   }
 };
 
-
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-assertEqual(findKeyByValue(bestTVShowsByGenre, " "), undefined);
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn 9-9"), undefined);
+module.exports = findKeyByValue;
