eab1b3e1880b444cf40f2adcb242c49c54008c89
Aden <aden.mccarville@gmail.com>
2023-10-23 21:45:42 -0400

    Updated index to include all Lotide functions

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
+const assertObjectsEqual = require('./assertObjectsEqual');
 const assertArraysEqual = require('./assertArraysEqual');
 const assertEqual = require('./assertEqual');
+const eqObjects = require('./eqObjects');
+const eqArrays = require('./eqArrays');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
 
 module.exports = {
   head:   head,
   tail:   tail,
   middle: middle,
   assertArraysEqual: assertArraysEqual,
-  assertEqual: assertEqual
+  assertEqual: assertEqual,
+  assertObjectsEqual: assertObjectsEqual,
+  eqObjects: eqObjects,
+  eqArrays: eqArrays,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  letterPositions: letterPositions,
+  takeUntil: takeUntil,
+  without: without
 };
