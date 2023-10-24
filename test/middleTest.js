aeb1e485f70bb6e380609c74389e3ec58f83735a
Aden <aden.mccarville@gmail.com>
2023-10-23 20:45:34 -0400

    Refactored middleTest to make use of Mocha & Chai for testing

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,11 +1,20 @@
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
-// Test code using assertArraysEqual to check returned array against expected result -- all tests should Pass
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-// Testing to ensure arrays with 2 or fewer elements result in an empty array being returned
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
+describe("#middle", () => {
+  it("Returns a single element ('2') when passed an array with an odd number of elements ([1,2,3])", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it("returns two elements ('2', '3') when passed an array with an even number of elements ([1,2,3,4])", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+  it("returns an empty array when passed an array with only 2 elements", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+
+  it("returns an empty array when passed an array with 1 element", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+});
