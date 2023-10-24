42de14e17220fc7ec45f65d8b054197d8168813a
Aden <aden.mccarville@gmail.com>
2023-10-23 20:44:38 -0400

    Refactored tailTest to make use of Mocha & Chai for testing

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,21 +1,16 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-// Test code 1: ensuring the initial array has not been altered
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
 
-// Test code 2: checking individual returned elements
-const words2 = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(words2.length, 2); // ensuring only two elements have been returned
-assertEqual(words2[0], "Lighthouse"); // checking the first element
-assertEqual(words2[1], "Labs"); // checking second element
+  it("returns an empty array when passed an array with only one element", () => {
+    assert.deepEqual(tail(["Test"]), []);
+  });
 
-// Test code 3: checking the results of entering an array with one element -- should return an empty array
-const test3 = tail(["RemoveMe"]);
-assertEqual(test3.length, 0);
-
-// Test code 4: checking the results of entering an empty array -- should again return an empty array
-const test4 = tail([]);
-assertEqual(test4.length, 0);
+  it("returns an empty array when passed an empty array", () => {
+    assert.deepEqual(tail([]), []);
+  });
+});
