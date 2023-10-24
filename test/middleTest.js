463fd355d901b4215eb162cc77b9194ec822ec24
Aden <aden.mccarville@gmail.com>
2023-10-23 20:51:56 -0400

    Added test to ensure original array is not altered by middle function

--- test/middleTest.js
+++ test/middleTest.js
@@ -2,6 +2,12 @@ const middle = require('../middle');
 const assert = require('chai').assert;
 
 describe("#middle", () => {
+  it("does not alter the passed-in array", () => {
+    const testArr = [1, 2, 3, 4, 5];
+    const middleArr = middle(testArr);
+    assert.strictEqual(testArr.length, 5);
+  });
+
   it("Returns a single element ('2') when passed an array with an odd number of elements ([1,2,3])", () => {
     assert.deepEqual(middle([1, 2, 3]), [2]);
   });
