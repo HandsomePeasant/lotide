9b10672cf831236a341cd894a096ad0907b29c4e
Aden <aden.mccarville@gmail.com>
2023-10-23 20:54:25 -0400

    Added test to ensure passed-in array is not altered by tail function

--- test/tailTest.js
+++ test/tailTest.js
@@ -2,6 +2,12 @@ const assert = require('chai').assert;
 const tail = require('../tail');
 
 describe("#tail", () => {
+  it("does not alter the passed-in array", () => {
+    const testArr = [1, 2, 3, 4, 5];
+    const tailArr = tail(testArr);
+    assert.strictEqual(testArr.length, 5);
+  });
+
   it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
     assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
   });
